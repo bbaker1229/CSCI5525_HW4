f806894c7c3c46abb96e61f0580d85ba3fb14225
Bryan Baker <bbaker1229@gmail.com>
2020-04-09 02:25:17 -0500

    Added some thoughts for adaboost and boosting regression.  None of it works right now.

--- hw4_adaboost.py
+++ hw4_adaboost.py
@@ -6,6 +6,8 @@
 # File Name: hw4_adaboost.py
 ############################################
 
+# none of this works yet
+
 import numpy as np
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
@@ -14,14 +16,40 @@ import matplotlib.pyplot as plt
 training_data = pd.read_csv("cancer_train.csv")
 train_y = np.array(training_data["y"])
 train_y = train_y.reshape(-1, 1)
+train_y[train_y == 0] = -1
 train_x = training_data
 train_x = train_x.drop("y", axis=1)
 testing_data = pd.read_csv("cancer_test.csv")
 test_y = np.array(testing_data["y"])
 test_y = test_y.reshape(-1, 1)
+test_y[test_y == 0] = -1
 test_x = testing_data
 test_x = test_x.drop("y", axis=1)
 
 decision_tree = DecisionTreeClassifier(max_depth=1)
 decision_tree = decision_tree.fit(train_x, train_y)
 decision_tree.predict(test_x)
+
+D_train = np.full(train_y.shape, 1/train_y.shape[0]).reshape(-1)
+D_test = np.full(test_y.shape, 1/test_y.shape[0]).reshape(-1)
+trees = []
+trees.append(decision_tree)
+alphas = []
+epsilon = np.sum(D_train * (decision_tree.predict(train_x) != train_y.reshape(-1)))
+print(epsilon)
+for t in range(100):
+    epsilon = np.sum(D_train * (decision_tree.predict((D_train * train_y.reshape(-1)).reshape(-1,1)) != train_y.reshape(-1)))
+    print(epsilon)
+    alpha = 0.5 * np.log((1 - epsilon) / (epsilon))
+    alphas.append(alpha)
+    D_train = D_train * np.exp(-alpha * train_y.reshape(-1) * decision_tree.predict(train_x))
+    decision_tree = DecisionTreeClassifier(max_depth=1)
+    decision_tree = decision_tree.fit((D_train * train_y.reshape(-1)).reshape(-1,1), train_y)
+    decision_tree.predict((D_train * train_y.reshape(-1)).reshape(-1,1))
+    trees.append(decision_tree)
+
+    D_train = D_train * np.exp(-alpha * train_y.reshape(-1) * decision_tree.predict(train_x))
+
+
+
+D.reshape(-1) * (trees[1].predict(train_x) != train_y.reshape(-1))
--- hw4_boosting_regression.py
+++ hw4_boosting_regression.py
@@ -6,6 +6,8 @@
 # File Name: hw4_boosting_regression.py
 ############################################
 
+# None of this works yet
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -13,6 +15,47 @@ import test_score as ts
 
 # Example of how the score function works
 N = 21283
+alpha = 0.01
 test_predictions = np.random.random(N)
 test_predictions = test_predictions.reshape(N, 1)
-ts.score(test_predictions)
+new_score = ts.score(test_predictions)
+new_score
+new_scores = np.apply_along_axis(ts.score, 1, test_predictions)
+new_scores = new_scores.reshape(-1, 1)
+new_scores
+test_predictions = test_predictions - alpha * new_scores
+# old_score = new_score - 1
+# while new_score > old_score:
+for j in range(22):
+    old_preds = test_predictions
+    old_score = new_score
+    old_scores = new_scores
+    # for i in range(N):
+    #    test_predictions[i] = test_predictions[i] - alpha * ts.score(test_predictions[i])
+    new_scores = np.apply_along_axis(ts.score, 1, test_predictions)
+    new_scores = new_scores.reshape(-1, 1)
+    if j == 0:
+        test_predictions = test_predictions - alpha * new_scores
+    else:
+        test_predictions = test_predictions - alpha * (new_scores - old_scores)
+    new_score = ts.score(test_predictions)
+    print(new_score)
+
+np.apply_along_axis(ts.score, 1, test_predictions)
+
+N = 21283
+alpha = 0.01
+test_1 = np.random.random(N)
+test_1 = test_1.reshape(N, 1)
+test_2 = np.random.random(N)
+test_2 = test_2.reshape(N, 1)
+
+h = (ts.score(test_2) - ts.score(test_1)) / (test_2 - test_1)
+# test_1 = test_2
+for i in range(10):
+    temp = test_2
+    test_2 = test_1 - alpha * h
+    test_2 = test_2.clip(min=0)
+    test_1 = temp
+    h = (ts.score(test_2) - ts.score(test_1)) / (test_2 - test_1)
+    print(ts.score(test_2))
