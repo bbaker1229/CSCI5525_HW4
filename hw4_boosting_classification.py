3f608b0654b36836f9489c04d0564ecb9d712d14
Bryan John Baker <bake1358@umn.edu>
2020-04-11 20:49:17 -0500

    Fixed regression file.  Added initial try to classification file.  Need to finish the classification file.

--- hw4_boosting_classification.py
+++ hw4_boosting_classification.py
@@ -6,6 +6,8 @@
 # File Name: hw4_boosting_classification.py
 ############################################
 
+# Need to get this to work
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -17,15 +19,41 @@ test_predictions = np.random.random(N)
 test_predictions = test_predictions.reshape(N, 1)
 ts.error_rate(test_predictions)
 
-# Examples of how Alpha and E works for boosting - Delete later.
-np.array([1.0, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.0]) / 10
+N = 21283
+alpha = 0.1
+theta = 0.001
+new_predictions = np.random.random(N)
+new_predictions = new_predictions.reshape(N, 1)
+print(ts.error_rate(new_predictions))
+new_error = ts.error_rate(new_predictions)
+old_error = 100
+constant_val = np.sum(new_predictions**2)
+i = 1
+iter = []
+error_lst = []
+# for i in range(10):
+while np.abs(old_error - new_error) > 0.00001:
+    old_error = new_error
+    old_predictions = new_predictions
+    error_0 = np.apply_along_axis(ts.error_rate, 1, old_predictions)
+    error_0 = error_0.reshape(-1, 1)
+    error_theta = np.apply_along_axis(ts.error_rate, 1, old_predictions + theta)
+    error_theta = error_theta.reshape(-1, 1)
+    delta_loss = (error_theta - error_0) / theta
+    new_predictions = old_predictions - alpha * delta_loss
+    new_predictions = new_predictions * np.sqrt(constant_val / np.sum(new_predictions ** 2))
+    print(ts.error_rate(new_predictions))
+    new_error = ts.error_rate(new_predictions)
+    iter.append(i)
+    i += 1
+    error_lst.append(new_error)
+    # print(iter)
+    # print(error_lst)
 
-D1 = np.array([1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0])
-y = np.array([1.0, -1.0, 1.0, -1.0, 1.0, -1.0, 1.0, -1.0, 1.0, -1.0])
-h1 = np.array([1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, 1.0, -1.0])
-
-D2 = D1 * np.exp(-0.42 * y * h1)
-
-h2 = np.array([1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, -1.0, 1.0, -1.0])
-
-D3 = D2 * np.exp(-0.65 * y * h2)
+# Create the misclassification plot
+plt.xlabel('Number of Weak Learners')
+plt.ylabel('Error')
+plt.title('Error Improvement based on boosting')
+plt.grid()
+plt.plot(error_lst)
+plt.show()
--- hw4_boosting_regression.py
+++ hw4_boosting_regression.py
@@ -16,46 +16,78 @@ import test_score as ts
 # Example of how the score function works
 N = 21283
 alpha = 0.01
-test_predictions = np.random.random(N)
-test_predictions = test_predictions.reshape(N, 1)
-new_score = ts.score(test_predictions)
-new_score
-new_scores = np.apply_along_axis(ts.score, 1, test_predictions)
-new_scores = new_scores.reshape(-1, 1)
-new_scores
-test_predictions = test_predictions - alpha * new_scores
-# old_score = new_score - 1
-# while new_score > old_score:
-for j in range(22):
-    old_preds = test_predictions
-    old_score = new_score
-    old_scores = new_scores
-    # for i in range(N):
-    #    test_predictions[i] = test_predictions[i] - alpha * ts.score(test_predictions[i])
-    new_scores = np.apply_along_axis(ts.score, 1, test_predictions)
-    new_scores = new_scores.reshape(-1, 1)
-    if j == 0:
-        test_predictions = test_predictions - alpha * new_scores
-    else:
-        test_predictions = test_predictions - alpha * (new_scores - old_scores)
-    new_score = ts.score(test_predictions)
-    print(new_score)
+theta = 0.01
 
-np.apply_along_axis(ts.score, 1, test_predictions)
+new_predictions = np.random.random(N)
+new_predictions = new_predictions.reshape(N, 1)
+print(ts.score(new_predictions))
+new_score = ts.score(new_predictions)
+old_score = 100
+i = 1
+iter = []
+scores_lst = []
+# for i in range(10):
+while np.abs(old_score - new_score) > 0.00001:
+    old_score = new_score
+    old_predictions = new_predictions
+    scores_0 = np.apply_along_axis(ts.score, 1, old_predictions)
+    scores_0 = scores_0.reshape(-1, 1)
+    scores_theta = np.apply_along_axis(ts.score, 1, old_predictions + theta)
+    scores_theta = scores_theta.reshape(-1, 1)
+    delta_loss = (scores_theta - scores_0) / theta
+    new_predictions = old_predictions - alpha * delta_loss
+    print(ts.score(new_predictions))
+    new_score = ts.score(new_predictions)
+    iter.append(i)
+    i += 1
+    scores_lst.append(new_score)
+    # print(iter)
+    # print(scores_lst)
+
+# Create the misclassification plot
+plt.xlabel('Number of Weak Learners')
+plt.ylabel('Score')
+plt.title('Score Improvement based on boosting')
+plt.grid()
+plt.plot(scores_lst)
+plt.show()
+
 
 N = 21283
-alpha = 0.01
-test_1 = np.random.random(N)
-test_1 = test_1.reshape(N, 1)
-test_2 = np.random.random(N)
-test_2 = test_2.reshape(N, 1)
+alpha = 0.1
+theta = 0.001
+new_predictions = np.random.random(N)
+new_predictions = new_predictions.reshape(N, 1)
+print(ts.score(new_predictions))
+new_score = ts.score(new_predictions)
+old_score = 100
+constant_val = np.sum(new_predictions**2)
+i = 1
+iter = []
+scores_lst = []
+# for i in range(10):
+while np.abs(old_score - new_score) > 0.00001:
+    old_score = new_score
+    old_predictions = new_predictions
+    scores_0 = np.apply_along_axis(ts.score, 1, old_predictions)
+    scores_0 = scores_0.reshape(-1, 1)
+    scores_theta = np.apply_along_axis(ts.score, 1, old_predictions + theta)
+    scores_theta = scores_theta.reshape(-1, 1)
+    delta_loss = (scores_theta - scores_0) / theta
+    new_predictions = old_predictions - alpha * delta_loss
+    new_predictions = new_predictions * np.sqrt(constant_val / np.sum(new_predictions ** 2))
+    print(ts.score(new_predictions))
+    new_score = ts.score(new_predictions)
+    iter.append(i)
+    i += 1
+    scores_lst.append(new_score)
+    # print(iter)
+    # print(scores_lst)
 
-h = (ts.score(test_2) - ts.score(test_1)) / (test_2 - test_1)
-# test_1 = test_2
-for i in range(10):
-    temp = test_2
-    test_2 = test_1 - alpha * h
-    test_2 = test_2.clip(min=0)
-    test_1 = temp
-    h = (ts.score(test_2) - ts.score(test_1)) / (test_2 - test_1)
-    print(ts.score(test_2))
+# Create the misclassification plot
+plt.xlabel('Number of Weak Learners')
+plt.ylabel('Score')
+plt.title('Score Improvement based on boosting')
+plt.grid()
+plt.plot(scores_lst)
+plt.show()
